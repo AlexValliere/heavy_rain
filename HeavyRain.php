24e7fcc6b3dfb4ba72cc888139e0230390aefa82
Alexandre Petit <AlexandrePetit@users.noreply.github.com>
2019-06-15 13:31:14 +0200

    Added a HTML city viewer to help visualize the city better.

--- HeavyRain.php
+++ HeavyRain.php
@@ -17,6 +17,44 @@ class HeavyRain extends CityBuilder
 
         /* add your code here */
 
+        $this->drawCity($city);
+
         echo json_encode($city) . " => " . $result . "\n";
+    }
+
+    public function drawCity($city) // HTML viewver for the city
+    {
+        $cityHeight = max($city);
+        $cityWidth = count($city);
+
+        echo '<table>';
+
+        for ($i = 0; $i < $cityHeight; $i++)
+        {
+            echo '<tr>';
+
+            for ($j = 0; $j < $cityWidth; $j++)
+            {
+                if ($city[$j] >= $cityHeight - $i)
+                {
+                    echo '<td style="border: 1px solid #45811e; background-color: #85d652; width: 10px; height: 10px;"></td>';
+                }
+                else
+                {
+                    echo '<td style="border: 1px solid #ffffff; width: 10px; height: 10px;"></td>';
+                }
+            }
+
+            echo '</tr>';
+        }
+
+        echo '<tr>';
+        
+        for ($j = 0; $j < $cityWidth; $j++)
+        {
+            echo '<td style="text-align: center; border-top: 5px solid #2092f4;">' . ($j % 10) . '</td>';
+        }
+
+        echo '</tr></table>';
     }
 }
